a413579b641f9e8f7e0ae77362fc70cca385d989
amegala84-cell <amegala84@gmail.com>
2026-06-02 16:49:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,27 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Welcome to Sakshain!</h1>
-    <p>My first cloud website.</p>
+    <h1>Welcome to Sakshain</h1>
+    <p>My first cloud website!</p>
+    <a href='/about'>About</a><br>
+    <a href='/contact'>Contact</a>
     """
+
+@app.route("/about")
+def about():
+    return """
+    <h1>About Us</h1>
+    <p>This website is hosted on Render using Python Flask.</p>
+    <a href='/'>Home</a>
+    """
+
+@app.route("/contact")
+def contact():
+    return """
+    <h1>Contact</h1>
+    <p>Email: example@email.com</p>
+    <a href='/'>Home</a>
+    """
+
+if __name__ == "__main__":
+    app.run()
